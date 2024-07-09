bf012e7d523d9c7a58d298342b134dc9bc6b326c
counter-cowboy <79208562003@ya.ru>
2024-07-09 18:08:10 +0300

    docs: apiResource

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,14 @@
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('tasks')
-    ->group(function (){
-        Route::get('/', [TaskController::class, 'Index']);
-        Route::get('/{task}', [TaskController::class, 'Show']);
-        Route::post('/',[TaskController::class, 'Store']);
-        Route::patch('/{task}', [TaskController::class, 'Update']);
-        Route::delete('/{task}', [TaskController::class, 'Destroy']);
-    });
+Route::prefix('tasks')->apiResource('tasks', TaskController::class );
+
+//  ======We can make it harder, but readable======
+//
+//    ->group(function (){
+//        Route::get('/', [TaskController::class, 'Index']);
+//        Route::get('/{task}', [TaskController::class, 'Show']);
+//        Route::post('/',[TaskController::class, 'Store']);
+//        Route::patch('/{task}', [TaskController::class, 'Update']);
+//        Route::delete('/{task}', [TaskController::class, 'Destroy']);
+//    });
